bd545d1e210bb7230fea9dd535e73efa6b7ca03e
Marwan <marwanmamdouh159@gmail.com>
2025-12-18 01:34:39 +0200

    cancel send emails

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,6 @@ import Skills from "@/components/Skills";
 import { useEffect } from "react";
 
 const Home = () => {
-  useEffect(() => {
-    sendMail();
-  }, []);
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
